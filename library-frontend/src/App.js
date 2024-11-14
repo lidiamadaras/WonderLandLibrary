6895394b394874109db928cefa605bc6a839b6bc
lidiamadaras <125455921+lidiamadaras@users.noreply.github.com>
2024-11-15 00:00:30 +0200

    Nav working, searchbar added, better design

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -3,35 +3,19 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home'; // The home page
 import About from './pages/About'; // The about page
 import Login from './pages/Login'; // The login page
+import Main from './pages/Main'; // The login page
+import './App.css';
+
+
 
 function App() {
   return (
     <Router>
-      <div>
-        <h1>Welcome to the Library</h1>
-        <p>This is the home page of our library website where you can explore our collection and services.</p>
+      <div class="app-title">
+        <h1>Wonderland Library</h1>
+        <Main /> 
 
-        {/* Navigation Menu */}
-        <nav>
-          <ul style={{ listStyle: 'none', padding: 0, display: 'flex', gap: '1rem' }}>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
 
-        {/* Routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
       </div>
     </Router>
   );
--- library-frontend/src/pages/Main.js
+++ library-frontend/src/pages/Main.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import NavBar from './NavBar';
+import NavBar from '../components/NavBar';
 import About from './About';
 import Login from './Login';
+import '../css/NavBar.css';
 
 function Main() {
   return (
     <div>
-      <NavBar /> {/* Include NavBar here */}
-      <h1>Welcome to the Library</h1>
-      <p>This is the home page of our library website where you can explore our collection and services.</p>
+      <NavBar />
 
-      {/* Page Routes */}
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Show Home by default */}
+        <Route path="/" element={<Home />} index />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
       </Routes>
+        
     </div>
+    
   );
 }
 
