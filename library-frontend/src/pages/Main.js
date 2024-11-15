2296751b64ef03ef965ee931136633127c7bd3f6
lidiamadaras <125455921+lidiamadaras@users.noreply.github.com>
2024-11-15 10:36:16 +0200

    Layout for whole website

--- library-frontend/src/pages/Main.js
+++ library-frontend/src/pages/Main.js
@@ -5,18 +5,34 @@ import NavBar from '../components/NavBar';
 import About from './About';
 import Login from './Login';
 import '../css/NavBar.css';
+import '../css/Main.css';
 
 function Main() {
   return (
     <div>
       <NavBar />
-
-      <Routes>
-        <Route path="/" element={<Home />} index />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <div className="layout-container">
         
+        <div className="sidebar-left">
+          
+        </div>
+
+        
+        <div className="main-content">
+          <div className="box">
+            <Routes>
+              <Route path="/" element={<Home />} index />
+              <Route path="/login" element={<Login />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </div>
+
+        
+        <div className="sidebar-right">
+          
+        </div>
+      </div>
     </div>
     
   );
